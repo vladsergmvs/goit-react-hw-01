52aedf094a2961542dd665383312611cee410903
Volodymyr <vladsergmvs@gmail.com>
2024-12-22 16:59:28 +0200

    markup

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,37 +1,15 @@
-const FriendList = ({ name, tag, location, image, stats:{followers, views, likes} }) => {
-    return (
-      <div>
-  
-        <div>
-      
-          <img
-            src={image}
-            alt="User avatar"
-          />
-           <p>{name}</p>
-           <p>@{tag}</p>
-           <p>{location}</p>
-        </div>
-      
-        <ul>
-        
-          <li>
-             <span>Followers</span> <span>{followers}</span>
-          </li>
-          
-          <li>
-            <span>Views</span> <span>{views}</span>
-          </li>
-         
-          <li>
-             <span>Likes</span> <span>{likes}</span> 
-          </li>
-          
-        </ul>
-      </div>
-    );
-  };
-  
-  export default FriendList;
-  
-  
+import FriendListItem from "../FriendListItem/FriendListItem";
+
+const FriendList = ({ friends }) => {
+  return (
+    <ul>
+      {friends.map((friend) => (
+        <li key={friend.id}>
+          <FriendListItem {...friend} />
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default FriendList;
